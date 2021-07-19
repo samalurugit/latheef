96cce96d56426f11a62f32b2e72e63e6a228ee6e
asif <dlatheef@gmail.com>
2021-07-19 13:50:52 +0530

    Hi Latheef this pawan i have added fewcode line to exiting script pls check

--- script.sh
+++ script.sh
@@ -1,2 +1,8 @@
 this is my my fist git file 
+
  Today we are learning new commands from linux server
+##########################################################
+
+echo " enter the name collage name "
+echo " enter the school name "
+
